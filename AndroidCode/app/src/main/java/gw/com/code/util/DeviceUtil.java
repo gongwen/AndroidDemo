2c6d14716f3c6fe04586181c9f80c895a4fb74ce
gongwen <1798550470@qq.com>
2016-10-24 10:55:17 +0800

    update DeviceUtil

--- AndroidCode/app/src/main/java/gw/com/code/util/DeviceUtil.java
+++ AndroidCode/app/src/main/java/gw/com/code/util/DeviceUtil.java
@@ -1,6 +1,7 @@
 package gw.com.code.util;
 
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 import gw.com.code.MainApplication;
 
@@ -9,27 +10,66 @@ import gw.com.code.MainApplication;
  */
 
 public class DeviceUtil {
-    private static DisplayMetrics displayMetrics;
+    private static DisplayMetrics metrics;
 
     public static int getScreenWidth() {
-        if (displayMetrics == null) {
-            displayMetrics = MainApplication.getInstance().getResources().getDisplayMetrics();
+        if (metrics == null) {
+            metrics = MainApplication.getInstance().getResources().getDisplayMetrics();
         }
-        return displayMetrics.widthPixels;
+        return metrics.widthPixels;
     }
 
     public static int getScreeHeight() {
-        if (displayMetrics == null) {
-            displayMetrics = MainApplication.getInstance().getResources().getDisplayMetrics();
+        if (metrics == null) {
+            metrics = MainApplication.getInstance().getResources().getDisplayMetrics();
         }
-        return displayMetrics.heightPixels;
+        return metrics.heightPixels;
     }
 
     public static float getScreenDensity() {
-        if (displayMetrics == null) {
-            displayMetrics = MainApplication.getInstance().getResources().getDisplayMetrics();
+        if (metrics == null) {
+            metrics = MainApplication.getInstance().getResources().getDisplayMetrics();
         }
-        return displayMetrics.density;
+        return metrics.density;
     }
 
+    public static float px2dp(float value) {
+        return px2other(TypedValue.COMPLEX_UNIT_DIP, value);
+    }
+
+    public static float dp2px(float value) {
+        return other2px(TypedValue.COMPLEX_UNIT_DIP, value);
+    }
+
+    public static float sp2px(float value) {
+        return other2px(TypedValue.COMPLEX_UNIT_SP, value);
+    }
+
+    public static float other2px(int unit, float value) {
+        if (metrics == null) {
+            metrics = MainApplication.getInstance().getResources().getDisplayMetrics();
+        }
+        return TypedValue.applyDimension(unit, value, metrics);
+    }
+
+    public static float px2other(int unit, float value) {
+        if (metrics == null) {
+            metrics = MainApplication.getInstance().getResources().getDisplayMetrics();
+        }
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value / metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value / metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value / (metrics.xdpi * (1.0f / 72));
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value / metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value / (metrics.xdpi * (1.0f / 25.4f));
+        }
+        return 0;
+    }
 }
